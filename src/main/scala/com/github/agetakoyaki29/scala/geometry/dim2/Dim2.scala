64493ca52063e940f9ba0391f59671110a58d2f5
Your Name <you@example.com>
2017-01-14 03:19:52 +0900

    make Dim2Factory.scala

--- src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/Dim2.scala
+++ src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/Dim2.scala
@@ -1,11 +1,9 @@
 package com.github.agetakoyaki29.scala.geometry.dim2
 
-import scala.reflect.ClassTag
-
 import com.github.agetakoyaki29.scala.sameret.{SameRet, UpRet}
 import com.github.agetakoyaki29.scala.geometry
 import geometry.Delta._
-import geometry.{Dim, DimFactory}
+import geometry.Dim
 
 
 object Dim2 extends Dim2Factory[Dim2] {
@@ -99,18 +97,5 @@ class Dim2(_x: Double, val _y: Double) extends IndexedSeq[Double] with Dim {
     case dim2: Dim2 => this.zipmap(dim2) {_==_} reduce {_||_}
     case _ => false
   }
-}
 
-
-abstract class Dim2Factory[+T <: Dim2 : ClassTag] extends DimFactory[T] {
-  final val Length: Int = 2
-
-  def apply(x: Double, y: Double): T
-  def apply(op: Dim2): T = clone(op)
-  def apply(seq: Seq[Double]): T = {
-    require(seq.length == Length, s"wrong length seq; found: ${seq.length}, required: ${Length}")
-    this(seq(0), seq(1))
-  }
-
-  def clone(dim2: Dim2): T = ???
 }
